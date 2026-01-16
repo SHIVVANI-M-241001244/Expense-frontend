4a5248c7b3c4f202de43b6bcacbd658b8b5475ef
Shivvani <shivvani.m.2024.it@rajalakshmi.edu.in>
2026-01-16 23:48:58 +0530

    Frontend expense tracker

---
+++ js/api.js
@@ -0,0 +1,20 @@
+const API_URL = "http://localhost:5000/api/expenses";
+
+// Add expense
+export async function addExpense(expense) {
+  const response = await fetch(`${API_URL}/add`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(expense)
+  });
+
+  return response.json();
+}
+
+// Get all expenses
+export async function getExpenses() {
+  const response = await fetch(API_URL);
+  return response.json();
+}
