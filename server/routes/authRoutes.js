0d76f6a45fa7e2f4d27f83b98df9f45e0775dee5
Shivvani <shivvani.m.2024.it@rajalakshmi.edu.in>
2026-01-17 01:39:04 +0530

    Add auth routes

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,38 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+
+const router = express.Router();
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check if user already exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      userId: user._id,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -3,30 +3,18 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import expenseRoutes from "./routes/expenseroutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
-
-// Load environment variables (from root .env)
-dotenv.config({ path: "../.env" });
-
-// Connect to MongoDB
+dotenv.config();
 connectDB();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/expenses", expenseRoutes);
+app.use("/api/auth", authRoutes);
 
-// Test route (optional)
-app.get("/", (req, res) => {
-  res.send("Expense Tracker API is running 🚀");
-});
-
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
