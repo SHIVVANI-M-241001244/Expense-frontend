9cf1e9de396464f3b30f70e04c76124f20d773f6
Shivvani <shivvani.m.2024.it@rajalakshmi.edu.in>
2026-01-17 08:35:35 +0530

    Fix auth route filename casing

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import expenseRoutes from "./routes/expenseroutes.js";
+import expenseroutes from "./routes/expenseroutes.js";
 import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/expenses", expenseRoutes);
+app.use("/api/expenses", expenseroutes);
 app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 10000;
